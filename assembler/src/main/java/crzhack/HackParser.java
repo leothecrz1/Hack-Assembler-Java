1ff8222a4f75e529caf6dd367f3021e6a7aa3d30
Leonardo Davalos <32350741+leothecrz@users.noreply.github.com>
2023-10-13 11:26:21 -0700

    Parser Finished -- NEEDS TESTING

--- assembler/src/main/java/crzhack/HackParser.java
+++ assembler/src/main/java/crzhack/HackParser.java
@@ -23,8 +23,8 @@ public class HackParser
 
     private int lineNumber;
 
-    private File inputFile;
     private FileReader inputReader;
+    private BufferedReader bufferedReader;
 
     public HackParser(String filepath)
     {
@@ -34,14 +34,23 @@ public class HackParser
         try 
         {
             inputReader = new FileReader( new File(filepath) );
+            bufferedReader = new BufferedReader(inputReader);
         } 
         catch (FileNotFoundException e) 
         {
             e.printStackTrace();
         }
-
-
         fillSymbolTableDefaults();
+        parseSymbols();
+    }
+
+    private String trimComments(String str)
+    {
+        int index = str.indexOf("//");
+        if(index == -1)
+            return str;
+
+        return str.substring(0, index);
     }
 
     private void fillSymbolTableDefaults()
@@ -71,9 +80,152 @@ public class HackParser
         symbolTable.put("KBD", 24576);
     } 
 
-    public bool isAtEndOfFile()
+    private void resetFields()
     {
-        inputReader.
+        symbol = "";
+        comp = "";
+        dest = "";
+        jump = "";
+        type = InstructionTypes.UNSET;
+    }
+
+    private void AINS()
+    {
+        activeLine = trimComments(activeLine);
+        activeLine.trim();
+        type = InstructionTypes.A_INSTRUCTION;
+        symbol = activeLine.substring(1);
+    }
+
+    private void CINS()
+    {
+        activeLine = trimComments(activeLine);
+        activeLine.trim();
+        type = InstructionTypes.C_INSTRUCTION;
+
+        int destBreakPoint = activeLine.indexOf('=');
+        int jumpBreakPoint = activeLine.indexOf(';');
+
+        if(destBreakPoint == -1)
+        {
+            if(jumpBreakPoint == -1)
+            {
+                comp = activeLine;
+                return;
+            }
+
+            comp = activeLine.substring(0, jumpBreakPoint);
+            jump = activeLine.substring(jumpBreakPoint+1);
+            return;
+        }
+
+        if(jumpBreakPoint == -1)
+        {
+            dest = activeLine.substring(0, destBreakPoint);
+            comp = activeLine.substring(jumpBreakPoint+1);
+            return;
+        }
+
+        dest = activeLine.substring(0, destBreakPoint);
+        comp = activeLine.substring(destBreakPoint+1, jumpBreakPoint);
+        jump = activeLine.substring(jumpBreakPoint+1);
+
+    }
+
+    private void LINS()
+    {
+        activeLine = trimComments(activeLine);
+        activeLine = activeLine.trim();
+        type = InstructionTypes.L_INSTRUCTION;
+        symbol = activeLine.substring(1, activeLine.length()-2);
+    }
+
+    private void setInstruction()
+    {
+        activeLine = activeLine.trim();
+        resetFields();
+
+        if(activeLine.length() < 1)
+            return;
+
+        char firstCharacter = activeLine.charAt(0);
+        switch (firstCharacter) 
+        {
+            case '@':
+                lineNumber++;
+                AINS();
+                break;
+        
+            case '(':
+                LINS();
+                break;
+
+            case '/':
+                break;
+
+            default:
+                lineNumber++;
+                CINS();
+                break;
+        }
+    }
+
+    private void parseSymbols()
+    {
+        while(!isAtEndOfFile())
+        {
+            if(type == InstructionTypes.A_INSTRUCTION)
+            {
+                try {
+                    Integer.parseInt(symbol);
+                } catch (NumberFormatException ignore) {
+                    if( symbolTable.get(symbol) == null)
+                    {
+                        symbolTable.put(symbol, lineNumber);
+                    }
+                }
+            }
+
+            if(type != InstructionTypes.C_INSTRUCTION)
+                return;
+
+            int pIndex = activeLine.indexOf('(');
+            int pIndexTwo = activeLine.indexOf(')');
+            String str = activeLine.substring(pIndex+1, pIndexTwo).trim();
+            
+            if( symbolTable.put(str, lineNumber) == null )
+                System.out.println(symbol + " " + lineNumber);
+        }
+
+        try 
+        {
+            inputReader.reset();
+            activeLine = "";
+            resetFields();
+            lineNumber = 0;
+        } 
+        catch (IOException e) 
+        {
+            e.printStackTrace();
+        }
+
+    }
+
+    public boolean isAtEndOfFile()
+    {
+        try 
+        {
+            if( (activeLine = bufferedReader.readLine()) != null)
+            {
+                setInstruction();
+                return false;
+            }
+        } 
+        catch (IOException e) 
+        {
+            e.printStackTrace();
+        }
+        return true;
     }
 
     public int getLineNumber() 
